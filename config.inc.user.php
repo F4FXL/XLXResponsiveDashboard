38795459e18078a64f9e00505881cbe83dc983b1
Geoffrey Merck <F4FXL@users.noreply.github.com>
2020-04-12 07:57:04 +0200

    Add congig.inc.user.php

---
+++ config.inc.user.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* This were you should put all of your customisation, own logo, module names etc ...
+* Commented code is provided as an exmaple
+*/
+
+/*
+$PageOptions['LogoFile']                             = './img/logo_kav.png';
+
+$PageOptions['ModuleNames']['A']                     = 'Interlink XLX';
+$PageOptions['ModuleNames']['B']                     = 'France';
+$PageOptions['ModuleNames']['C']                     = 'Dstar/TG20867/YSF';
+$PageOptions['ModuleNames']['D']                     = 'MMDVM TG6 DMR';
+$PageOptions['ModuleNames']['E']                     = 'Auvergne-Rhone-Alpes';
+$PageOptions['ModuleNames']['F']                     = 'Bourgogne-Franche-Comté';
+$PageOptions['ModuleNames']['G']                     = 'Bretagne';
+$PageOptions['ModuleNames']['H']                     = 'Centre-Val de Loire';
+$PageOptions['ModuleNames']['I']                     = 'Corse';
+$PageOptions['ModuleNames']['J']                     = 'Grand Est';
+$PageOptions['ModuleNames']['K']                     = 'Hauts de France';
+$PageOptions['ModuleNames']['L']                     = 'Ile de France';
+$PageOptions['ModuleNames']['M']                     = 'Normandie';
+$PageOptions['ModuleNames']['N']                     = 'New Aquitaine';
+$PageOptions['ModuleNames']['O']                     = 'Occitanie';
+$PageOptions['ModuleNames']['P']                     = 'Pays de la Loire';
+$PageOptions['ModuleNames']['Q']                     = 'PACA';
+$PageOptions['ModuleNames']['R']                     = 'DOM-TOM';
+$PageOptions['ModuleNames']['S']                     = 'UNARAF';
+$PageOptions['ModuleNames']['T']                     = 'Test';
+$PageOptions['ModuleNames']['W']                     = 'World Wide';
+$PageOptions['ModuleNames']['X']                     = 'Sysop';
+*/
+?>
