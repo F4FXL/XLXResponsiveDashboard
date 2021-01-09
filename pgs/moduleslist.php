d79cb78627f343c3fe8f219ae81e231e20b400f5
Geoffrey Merck <F4FXL@users.noreply.github.com>
2021-01-09 22:25:45 +0100

    Add link  to modules

--- pgs/moduleslist.php
+++ pgs/moduleslist.php
@@ -19,7 +19,7 @@ foreach($PageOptions['ModuleNames'] as $module => $description)
 ?>
 <tr>
     <th scope="row"><?php echo $module;?></th>
-    <td><?php echo $description;?></td>
+    <td><a href="index.php?show=users&module=<?php echo $module;?>" class="pl"><?php echo strip_tags($description);?></a></td>
     <td><?php echo count($Reflector->GetStationsInModule($module));?></td>
     <td><?php echo count($Reflector->GetNodesInModulesById($module));?></td>
 </tr>
