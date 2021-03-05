0a6e1d3bb2cbd5eeb33f5687e349cee0c20c85ad
Geoffrey Merck <F4FXL@users.noreply.github.com>
2021-03-05 08:41:38 +0100

    Add pretty links

--- index.php
+++ index.php
@@ -75,6 +75,7 @@ if ($LastSync < (time() - $CallingHome['PushDelay'])) { $Ressource=@fopen($Calli
 <html lang=" en">
 
     <head>
+        <base href="/" /> 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
