20b9e23c04ab124c11ad91abfd633c286b1a9393
Geoffrey Merck <f4fxl@users.noreply.github.com>
2024-02-23 17:25:41 +0100

    Add meta information

--- config.inc.user.php
+++ config.inc.user.php
@@ -4,7 +4,14 @@
 * Commented code is provided as an exmaple
 */
 
-$PageOptions['xlxdVersion']                          = '<a href="https://github.com/F4FXL/xlxd/releases/tag/xlx208%2Fv2.4.1" target="_blank">xlx208/2.4.1</a>';	//xlxd version
+$PageOptions['MetaDescription']                      = 'Reflecteur et Serveur Starnet multi protocoles francophone, fourni par F5KAV';  // Meta Tag Values, usefull for Search Engine
+$PageOptions['MetaKeywords']                         = 'Ham Radio, D-Star, XReflector, XLX, XRF, DCS, REF, DMR, YSF';        // Meta Tag Values, usefull forSearch Engine
+$PageOptions['MetaAuthor']                           = 'F5KAV';                                                      // Meta Tag Values, usefull for Search Engine
+$PageOptions['MetaRevisit']                          = 'After 30 Days';                                              // Meta Tag Values, usefull for Search Engine
+$PageOptions['MetaRobots']                           = 'index,follow';                                               // Meta Tag Values, usefull for Search Engine
+
+$PageOptions['PageRefreshDelay']                     = '10000';		// Page refresh time in miliseconds
+$PageOptions['xlxdVersion']                          = '<a href="https://github.com/F4FXL/xlxd/releases/tag/xlx208%2Fv2.5.3" target="_blank">xlx208/2.5.3</a>';	//xlxd version
 
 $PageOptions['CustomTXT']                            = 'Sysop F5KAV R.A.C.C.W';					// custom text in your header   
 $PageOptions['CustomTXTLink']                        = 'https://www.f5kav.fr/nos-installations/reflecteur-xlx208/';
@@ -12,7 +19,7 @@ $PageOptions['LogoFile']                             = './img/logo_kav.png';
 
 $PageOptions['ModuleNames']['A']                     = 'Worldwide (Interlink)';
 $PageOptions['ModuleNames']['B']                     = 'France';
-$PageOptions['ModuleNames']['C']                     = 'DStar/TG20867 FreeDMR/<a href="http://ipsc2fr.dnsalias.net/ipsc/" target=_blank>TG867 IPSC2FR</a>/YSF';
+$PageOptions['ModuleNames']['C']                     = 'DStar/<a href="http://ipsc2fr.dnsalias.net/ipsc/" target=_blank>TG867 IPSC2FR</a>/YSF';
 $PageOptions['ModuleNames']['D']                     = 'Francophone';
 $PageOptions['ModuleNames']['E']                     = 'Auvergne-Rhone-Alpes';
 $PageOptions['ModuleNames']['F']                     = 'Bourgogne-Franche-Comté';
@@ -31,26 +38,31 @@ $PageOptions['ModuleNames']['R']                     = 'DOM-TOM';
 $PageOptions['ModuleNames']['S']                     = 'UNARAF';
 $PageOptions['ModuleNames']['T']                     = 'Test';
 $PageOptions['ModuleNames']['U']                     = 'Urgence';
+$PageOptions['ModuleNames']['V']                     = 'DRats & Fast Data';
 $PageOptions['ModuleNames']['W']                     = 'World Wide';
 $PageOptions['ModuleNames']['X']                     = 'Sysop';
 
+$PageOptions['PageRefreshDelay']                     = '5000';		// Page refresh time in miliseconds
+
 $PageOptions['SGS']['Show']                          = true;         // Show SGS page
+$PageOptions['SGS']['version']                       = "1.0";         // Show SGS page
+$PageOptions['SGS']['name']                          = "STN208";
 
 # sgs status page options
 $PageOptions['SGSTitle']                             = 'STN208 Smart Groups Status';
 $PageOptions['SGSServer']	                         = 'stn208'; //sgsremote <servername> as in sgsremote config
 $PageOptions['PageRefreshAlt']                       = '60000';      // Alternate page refresh time in miliseconds
-$PageOptions['SGSRepeaterReplace']['F1ZNZ   B']        = 'STN208  B';
-$PageOptions['SGSRepeaterReplace']['F1ZNZ   C']        = 'STN208  C';
-$PageOptions['SGSRepeaterReplace']['F1ZNZ   D']        = 'STN208  D';
+$PageOptions['RepeaterAlias']['F1ZNZ   B']           = 'STN208  B';
+$PageOptions['RepeaterAlias']['F1ZNZ   C']           = 'STN208  C';
+$PageOptions['RepeaterAlias']['F1ZNZ   D']           = 'STN208  D';
+$PageOptions['RepeaterAlias']['IPSC2FR B']           = 'IPSC2 France 2';
 
-
-$Service['AmbedStatusCommand']                       = "timeout -k 2s 2s socat -u -t1 'TCP4:10.9.0.100:6789' -";
+$Service['AmbedStatusCommand']                       = "timeout -k 2s 2s socat -u -t1 'TCP4:10.8.0.110:6789' -";
 
 $CallingHome['Active']                               = true;					               // xlx phone home, true or false
 $CallingHome['MyDashBoardURL']                       = 'http://xlx208.f5kav.fr';			       // dashboard url
 $CallingHome['PushDelay']                            = 600;  	                               // push delay in seconds
 $CallingHome['Country']                              = "France";                         // Country
-$CallingHome['Comment']                              = "French D-Star XLX Multiprotocol Reflector, brought to you by F5KAV";            // Comment. Max 100 character
-
+$CallingHome['Comment']                              = "French D-Star XLX Multiprotocol Reflector and Smart Group Server, brought to you by F5KAV";            // Comment. Max 100 character
+$CallingHome['OverrideIPAddress']                    = "213.186.34.50"; 
 ?>
